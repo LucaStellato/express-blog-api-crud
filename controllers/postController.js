502009d19f72733cbd7b6c90d0f98adc5f6eb2ac
LucaStellato <lucastellato435@gmail.com>
2025-11-26 11:33:54 +0100

    paste code

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,18 +1,20 @@
 const posts = require('../posts')
 
 function index(req, res) {
-
+    //res.send('Lista dei post')
+    res.json(posts);
 }
 function show(req, res) {
-
+    res.json(posts[req.params.id - 1])
 }
 function store(req, res) {
-
+    res.send('creazione nuovo post')
 }
 function update(req, res) {
-
+    res.send('modifica dei post')
 }
 function destroy(req, res) {
+    res.send('eliminazione post')
 
 }
 
