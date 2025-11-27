5f561b81621be13a995d88ee7bb9b9a9e908123b
LucaStellato <lucastellato435@gmail.com>
2025-11-27 12:14:17 +0100

    error title undefined

--- controllers/postController.js
+++ controllers/postController.js
@@ -24,6 +24,20 @@ function show(req, res) {
 }
 function store(req, res) {
     console.log(req.body)
+
+    const newId = posts[posts.length - 1].id + 1;
+    const newPost = {
+        id: newId,
+        title: req.body.title,
+        content: req.body.content,
+        image: req.body.image,
+        tags: req.body.tags
+    }
+    posts.push(newPost);
+    console.log(posts)
+    res.status(201)
+    res.json(newPost)
+
 }
 function update(req, res) {
     res.send('modifica dei post')
