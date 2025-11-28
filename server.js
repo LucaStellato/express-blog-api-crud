fb1708a373bc14c624bb74b299cd6c1271e98566
LucaStellato <lucastellato435@gmail.com>
2025-11-28 11:34:38 +0100

    import in server.js errorhandler

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 const postsRouter = require('./routers/posts')
+const errorHandler = require('./middleware/errorHandler')
 app.use(express.json());
+
 
 app.get('/', (req, res) => {
     res.send('post Api')
